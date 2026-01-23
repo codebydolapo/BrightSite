f526e96fce2d11d771b92119218e0cb7ab3e74d2
codebydolapo <bashorun115@gmail.com>
2026-01-23 20:13:49 +0100

    fixed gsap library import

--- lib/gsap.ts
+++ lib/gsap.ts
@@ -1,9 +1,9 @@
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"; // Using the dist version can sometimes be safer for SSR
 
 if (typeof window !== "undefined") {
   gsap.registerPlugin(ScrollTrigger);
 }
 
-export * from "gsap";
-export { ScrollTrigger };
+// Export both the main gsap object and the plugin
+export { gsap, ScrollTrigger };
